8f4289d10c72dd86a0af485bc0f14770e4ee8ce6
Vincent LIEGEOIS <vincent.liegeois@unamur.be>
2023-06-16 09:44:37 +0200

    Add functions to simdStatic + tests

--- Sources/simdStatic.swift
+++ Sources/simdStatic.swift
@@ -13,33 +13,237 @@ import Glibc
 
 // Will define all static helpers for simd
 
-// MARK: - Double
-
+// MARK: - Double/Float
 /// Reciprocal square root.
 public func rsqrt(_ x: Double) -> Double { return 1.0 / sqrt(x) }
 
-//public func rsqrt(_ x: Double) -> Double { // Not used as x = 25.0 result 0.19968952216623773 instead of 0.2
-//    let halfx = 0.5 * x
-//    var i = x.bitPattern
-//    i = 0x5fe6eb50c7b537a9 - (i >> 1)
-//    var y = Double(bitPattern: i)
-//    y = y * (1.5 - (halfx * y * y))
-//    return y
-//}
+/// Reciprocal square root.
+public func rsqrt(_ x: Float) -> Float { return 1.0 / sqrt(x) }
 
 // MARK: - simd_double3
-
+public func length_squared(_ __x: simd_double3) -> Double {simd_length_squared(__x)}
 public func simd_length_squared(_ __x: simd_double3) -> Double {
     return __x.x*__x.x + __x.y*__x.y + __x.z*__x.z
 }
 
+public func length(_ __x: simd_double3) -> Double {simd_length(__x)}
+public func simd_length(_ __x: simd_double3) -> Double {
+    return sqrt(simd_length_squared(__x))
+}
+
+public func normalize(_ __x: simd_double3) -> simd_double3 {simd_normalize(__x)}
 public func simd_normalize(_ __x: simd_double3) -> simd_double3 {
-    let magnitude = sqrt(simd_length_squared(__x))
+    let magnitude = simd_length(__x)
     return .init(__x.x / magnitude, __x.y / magnitude, __x.z / magnitude)
 }
 
+public func cross(_ __x: simd_double3, _ __y: simd_double3) -> simd_double3 { simd_cross(__x, __y)}
 public func simd_cross(_ __x: simd_double3, _ __y: simd_double3) -> simd_double3 {
     return .init(__x.y*__y.z - __x.z*__y.y,
                  __x.z*__y.x - __x.x*__y.z,
                  __x.x*__y.y - __x.y*__y.x)
 }
+
+// MARK: - simd_float3
+public func length_squared(_ __x: simd_float3) -> Float {simd_length_squared(__x)}
+public func simd_length_squared(_ __x: simd_float3) -> Float {
+    return __x.x*__x.x + __x.y*__x.y + __x.z*__x.z
+}
+
+public func length(_ __x: simd_float3) -> Float {simd_length(__x)}
+public func simd_length(_ __x: simd_float3) -> Float {
+    return sqrt(simd_length_squared(__x))
+}
+
+public func normalize(_ __x: simd_float3) -> simd_float3 {simd_normalize(__x)}
+public func simd_normalize(_ __x: simd_float3) -> simd_float3 {
+    let magnitude = simd_length(__x)
+    return .init(__x.x / magnitude, __x.y / magnitude, __x.z / magnitude)
+}
+
+public func cross(_ __x: simd_float3, _ __y: simd_float3) -> simd_float3 { simd_cross(__x, __y)}
+public func simd_cross(_ __x: simd_float3, _ __y: simd_float3) -> simd_float3 {
+    return .init(__x.y*__y.z - __x.z*__y.y,
+                 __x.z*__y.x - __x.x*__y.z,
+                 __x.x*__y.y - __x.y*__y.x)
+}
+
+// MARK: - simd_double4
+public func length_squared(_ __x: simd_double4) -> Double {simd_length_squared(__x)}
+public func simd_length_squared(_ __x: simd_double4) -> Double {
+    return __x.x*__x.x + __x.y*__x.y + __x.z*__x.z + __x.w*__x.w
+}
+public func length(_ __x: simd_double4) -> Double {simd_length(__x)}
+public func simd_length(_ __x: simd_double4) -> Double {
+    return sqrt(simd_length_squared(__x))
+}
+public func normalize(_ __x: simd_double4) -> simd_double4 {simd_normalize(__x)}
+public func simd_normalize(_ __x: simd_double4) -> simd_double4 {
+    let magnitude = simd_length(__x)
+    return .init(__x.x / magnitude, __x.y / magnitude, __x.z / magnitude, __x.w / magnitude)
+}
+
+// MARK: - simd_float4
+public func length_squared(_ __x: simd_float4) -> Float {simd_length_squared(__x)}
+public func simd_length_squared(_ __x: simd_float4) -> Float {
+    return __x.x*__x.x + __x.y*__x.y + __x.z*__x.z + __x.w*__x.w
+}
+public func length(_ __x: simd_float4) -> Float {simd_length(__x)}
+public func simd_length(_ __x: simd_float4) -> Float {
+    return sqrt(simd_length_squared(__x))
+}
+public func normalize(_ __x: simd_float4) -> simd_float4 {simd_normalize(__x)}
+public func simd_normalize(_ __x: simd_float4) -> simd_float4 {
+    let magnitude = simd_length(__x)
+    return .init(__x.x / magnitude, __x.y / magnitude, __x.z / magnitude, __x.w / magnitude)
+}
+
+// MARK: - dot
+/// Dot product of `x` and `y`.
+public func dot(_ __x: simd_double3, _ __y: simd_double3) -> Double {
+    __x.x*__y.x + __x.y*__y.y + __x.z*__y.z
+}
+/// Dot product of `x` and `y`.
+public func dot(_ __x: simd_float3, _ __y: simd_float3) -> Float {
+    __x.x*__y.x + __x.y*__y.y + __x.z*__y.z
+}
+/// Dot product of `x` and `y`.
+public func dot(_ __x: simd_double4, _ __y: simd_double4) -> Double {
+    __x.x*__y.x + __x.y*__y.y + __x.z*__y.z + __x.w*__y.w
+}
+/// Dot product of `x` and `y`.
+public func dot(_ __x: simd_float4, _ __y: simd_float4) -> Float {
+    __x.x*__y.x + __x.y*__y.y + __x.z*__y.z + __x.w*__y.w
+}
+
+// MARK: - project
+/// Projection of `x` onto `y`.
+public func project(_ __x: simd_double3, _ __y: simd_double3) -> simd_double3 {simd_project(__x, __y)}
+/// Projection of `x` onto `y`.
+public func simd_project(_ __x: simd_double3, _ __y: simd_double3) -> simd_double3 {
+    let n = simd_normalize(__y)
+    return dot(__x, n) * n
+}
+/// Projection of `x` onto `y`.
+public func project(_ __x: simd_float3, _ __y: simd_float3) -> simd_float3 {simd_project(__x, __y)}
+/// Projection of `x` onto `y`.
+public func simd_project(_ __x: simd_float3, _ __y: simd_float3) -> simd_float3 {
+    let n = simd_normalize(__y)
+    return dot(__x, n) * n
+}
+/// Projection of `x` onto `y`.
+public func project(_ __x: simd_double4, _ __y: simd_double4) -> simd_double4 {simd_project(__x, __y)}
+/// Projection of `x` onto `y`.
+public func simd_project(_ __x: simd_double4, _ __y: simd_double4) -> simd_double4 {
+    let n = simd_normalize(__y)
+    return dot(__x, n) * n
+}
+/// Projection of `x` onto `y`.
+public func project(_ __x: simd_float4, _ __y: simd_float4) -> simd_float4 {simd_project(__x, __y)}
+/// Projection of `x` onto `y`.
+public func simd_project(_ __x: simd_float4, _ __y: simd_float4) -> simd_float4 {
+    let n = simd_normalize(__y)
+    return dot(__x, n) * n
+}
+
+// MARK: - reflect
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func reflect(_ x: simd_double3, n: simd_double3) -> simd_double3 {
+    simd_reflect(x, n)
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func simd_reflect(_ __x: simd_double3, _ __n: simd_double3) -> simd_double3 {
+    return __x - 2 * dot(__x, __n) * __n
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func reflect(_ x: simd_float3, n: simd_float3) -> simd_float3 {
+    simd_reflect(x, n)
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func simd_reflect(_ __x: simd_float3, _ __n: simd_float3) -> simd_float3 {
+    return __x - 2 * dot(__x, __n) * __n
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func reflect(_ x: simd_double4, n: simd_double4) -> simd_double4 {
+    simd_reflect(x, n)
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func simd_reflect(_ __x: simd_double4, _ __n: simd_double4) -> simd_double4 {
+    return __x - 2 * dot(__x, __n) * __n
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func reflect(_ x: simd_float4, n: simd_float4) -> simd_float4 {
+    simd_reflect(x, n)
+}
+/// Reflects x through the plane perpendicular to the normal vector n.
+///
+/// `x` reflected through the hyperplane with unit normal vector `n`, passing
+/// through the origin.  E.g. if `x` is [1,2,3] and `n` is [0,0,1], the result
+/// is [1,2,-3].
+public func simd_reflect(_ __x: simd_float4, _ __n: simd_float4) -> simd_float4 {
+    return __x - 2 * dot(__x, __n) * __n
+}
+
+// MARK: - distance
+
+/// Distance between `x` and `y`.
+public func distance(_ x: simd_double3, _ y: simd_double3) -> Double {
+    sqrt(distance_squared(x, y))
+}
+/// Distance between `x` and `y`, squared.
+public func distance_squared(_ x: simd_double3, _ y: simd_double3) -> Double {
+    simd_length_squared(x-y)
+}
+/// Distance between `x` and `y`.
+public func distance(_ x: simd_float3, _ y: simd_float3) -> Float {
+    sqrt(distance_squared(x, y))
+}
+/// Distance between `x` and `y`, squared.
+public func distance_squared(_ x: simd_float3, _ y: simd_float3) -> Float {
+    simd_length_squared(x-y)
+}
+/// Distance between `x` and `y`.
+public func distance(_ x: simd_double4, _ y: simd_double4) -> Double {
+    sqrt(distance_squared(x, y))
+}
+/// Distance between `x` and `y`, squared.
+public func distance_squared(_ x: simd_double4, _ y: simd_double4) -> Double {
+    simd_length_squared(x-y)
+}
+/// Distance between `x` and `y`.
+public func distance(_ x: simd_float4, _ y: simd_float4) -> Float {
+    sqrt(distance_squared(x, y))
+}
+/// Distance between `x` and `y`, squared.
+public func distance_squared(_ x: simd_float4, _ y: simd_float4) -> Float {
+    simd_length_squared(x-y)
+}
+
---
+++ Tests/simdStaticTests.swift
@@ -0,0 +1,43 @@
+//
+//  simdStaticTests.swift
+//  SimdSwiftTests
+//
+//  Created by Vincent LIEGEOIS on 16/06/2023.
+//
+
+import XCTest
+//#if canImport(simd)
+//import simd
+//#else
+@testable import SimdSwift
+//#endif
+
+class simdStaticTests: XCTestCase {
+    func testSIMDStaticDot() throws {
+        let a = simd_double3(4.0, 2, 3)
+        let n = simd_double3(0.6, 0.8, 0)
+        let z = dot(a, n)
+        XCTAssertEqual(z, 4.0, accuracy: 1e-13)
+    }
+    func testSimdStaticProject() throws {
+        let a = simd_double3(1.0, 2, 3)
+        let n = simd_double3(6.0, 2, 0)
+        let z = project(a, n)
+        XCTAssertEqual(z, SIMD3(1.5, 0.5, 0.0), accuracy: 1e-13)
+    }
+    
+    func testSimdStaticReflect() throws {
+        let a = simd_double3(1.0, 2, 3)
+        let n = simd_double3(-1.0, 0, 0)
+        let z = reflect(a, n: n)
+        XCTAssertEqual(z, SIMD3(-1.0, 2, 3), accuracy: 1e-13)
+    }
+    
+    func testSimdStaticReflect2() throws {
+        let a = simd_double3(4.0, 2, 3)
+        let n = simd_double3(3.0, 4, 0)
+        let z = reflect(a, n: n)
+        XCTAssertEqual(z, SIMD3(-116.0, -158.0, 3.0), accuracy: 1e-13)
+    }
+    
+}
